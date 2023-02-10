d8e33deb7b7ab71cf3c2ededba6b3e09c0a4e02e
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-02-10 12:55:05 -0800

    updated nutritracker.urls to point /foods to foods.views.index()

--- nutritracker/urls.py
+++ nutritracker/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import foods.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('foods/', foods.views.index)
 ]
