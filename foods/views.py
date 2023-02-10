fa454278070fa917fe34fb69833eb04ed54e80c0
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-02-10 12:51:58 -0800

    added foods.views.index() using template rendering (buggy)

--- foods/views.py
+++ foods/views.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from .models import Food
+from utils import Food_Detailed
+
+
+def index(request):
+    template = loader.get_template('list_of_foods.html')
+    food_objs = [Food_Detailed.from_model_object(food_model_obj) for food_model_obj in Food.objects.filter()]
+    context = {'food_objs':food_objs}
+    return HttpResponse(template.render(context, request))
